f1686a2236f827a8275c0187e52cf67b87802ecd
sara <119621947+summiii@users.noreply.github.com>
2025-12-01 13:40:50 +0100

    Update HelloServlet.java

--- HelloServlet.java
+++ HelloServlet.java
@@ -5,9 +5,11 @@ import jakarta.servlet.http.*;
 import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
+
         resp.setContentType("text/html");
         resp.getWriter().println("<h1>Hello from Servlet !</h1>");
     }
